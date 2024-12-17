6825567e5af471f9191cc95bf6316b51b44d717a
Kenneth Ho <8515878+kenesu-h@users.noreply.github.com>
2024-12-16 19:17:35 -0500

    Create from Minecraft Development template

---
+++ src/main/kotlin/io/github/kenesu_h/mbtaCraftLivemap/MbtaCraftLivemap.kt
@@ -0,0 +1,14 @@
+package io.github.kenesu_h.mbtaCraftLivemap
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class MbtaCraftLivemap : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
